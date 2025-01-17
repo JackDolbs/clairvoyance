c8184d6d674f4d2cd6229ad27095505b213213d5
JackDolbs <hi@jackdolbs.me>
2025-01-17 12:36:31 +0000

    Change to layout.ts to allow us to access /login /register /forgot-password even when authenticated to build pages

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -8,13 +8,21 @@ export const load: LayoutLoad = async ({ url }) => {
         const isAuthenticated = authExpiry && new Date().getTime() < parseInt(authExpiry);
         const isAuthRoute = url.pathname.startsWith('/auth');
         
+        // List of routes we want to work on
+        const allowedAuthRoutes = [
+            '/auth/login',
+            '/auth/register',
+            '/auth/forgot-password'
+        ];
+        
         // If not authenticated and trying to access app routes
         if (!isAuthenticated && !isAuthRoute) {
             throw redirect(302, '/auth/protected');
         }
         
         // If authenticated and trying to access auth routes
-        if (isAuthenticated && isAuthRoute) {
+        // Only redirect if it's not one of our allowed routes
+        if (isAuthenticated && isAuthRoute && !allowedAuthRoutes.includes(url.pathname)) {
             throw redirect(302, '/');
         }
     }
