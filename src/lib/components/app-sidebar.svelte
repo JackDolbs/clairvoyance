8d38de01a6bcd5cf5a5706ef1f9dccd7d86b73b8
JackDolbs <hi@jackdolbs.me>
2025-01-28 13:57:18 +0000

    Fix small on:click > onclick error

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -124,7 +124,7 @@
 											<button 
 												{...props} 
 												class="flex items-center gap-2 mx-2 py-1.5 text-sm w-full"
-												on:click={() => {
+												onclick={() => {
 													if (item.title === "Dashboards") {
 														dashboardActive = !dashboardActive;
 													} else if (item.title === "Analytics") {
