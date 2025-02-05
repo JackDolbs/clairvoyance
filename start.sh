9c70d1f51aa5d7930564737f38fbfc7b134b9804
JackDolbs <hi@jackdolbs.me>
2025-02-05 16:33:27 +0000

    Update Vite to use production mode

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@
 ./data/pocketbase/pocketbase serve --http="0.0.0.0:8090" --dir="./data/pocketbase/pb_data" &
 
 # Start Vite
-npm run dev -- --host 0.0.0.0 --port 5174 
+npm run build
+npm run preview -- --host 0.0.0.0 --port 5174 
